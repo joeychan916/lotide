40bb0a58d56865722f6d89b54b9e49023270e5f3
Joey Chan <joeychan916@gmail.com>
2023-07-06 22:52:28 +0000

    Refractor head function and remove duplicate assertEqual code

--- head.js
+++ head.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   if (array.length === 0) {
@@ -13,8 +7,6 @@ const head = function(array) {
   return array[0];
 };
 
-// Test codes
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
 
 
