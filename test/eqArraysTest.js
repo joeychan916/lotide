284d037a7306c9ce1bfd47bc0df3219c42277243
Joey Chan <joeychan916@gmail.com>
2023-07-07 00:20:22 +0000

    Create eqArraysTest.js to seperate test code for eqArrays function

---
+++ test/eqArraysTest.js
@@ -0,0 +1,8 @@
+const assertEqual = require('./assertEqual');
+const eqArrays = require('./eqArrays');
+
+// Test cases
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // should pass
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // should fail
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // should pass
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // should fail
