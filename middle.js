be309737c5e4bab84edc31fbffbdc066cd92d5f4
Joey Chan <joeychan916@gmail.com>
2023-07-07 00:27:23 +0000

    Refactor middle.js to use modules and export middle function

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  }
-  for (let i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-
 const middle = function(array) {
   const middleIndex = Math.floor(array.length / 2);
   if (array.length <= 2) {
@@ -29,13 +9,8 @@ const middle = function(array) {
   }
 };
 
-// Test cases
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+module.exports = middle;
+
 
 
 
