946c54a1124178c61754f64ef2678464b484614e
Joey Chan <joeychan916@gmail.com>
2023-07-07 02:33:14 +0000

    Refactor head.js to use modules and add head function

--- head.js
+++ head.js
@@ -1,5 +1,3 @@
-const assertEqual = require('./assertEqual');
-
 const head = function(array) {
   if (array.length === 0) {
     return undefined;
@@ -8,5 +6,7 @@ const head = function(array) {
 };
 
 module.exports = head;
+
+
 
 
